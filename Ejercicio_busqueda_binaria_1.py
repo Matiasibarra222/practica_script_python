c718ad5fe5c43e7e104bd551ef8ebb5cf4e43c19
Matias <matiasiabrra222@gmail.com>
2020-06-28 13:01:38 -0400

    Ejercicio busqueda binaria 1

---
+++ Ejercicio_busqueda_binaria_1.py
@@ -0,0 +1,14 @@
+  GNU nano 4.9.2           Ejercicio_busqueda_binaria_1.py            Modified
+    return find_item(list[middle+1:], item)
+
+  return False
+
+#Do not edit below this line - This code helps check your work!
+list_of_names = ["Parker", "Drew", "Cameron", "Logan", "Alex", "Chris", "Terry">
+
+print(find_item(list_of_names, "Alex")) # True
+print(find_item(list_of_names, "Andrew")) # False
+print(find_item(list_of_names, "Drew")) # True
+print(find_item(list_of_names, "Jared")) # False
+
+
